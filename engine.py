8a2b257b5126b191b8b915335e5bfe5f105c13c2
Ryan Hurst <r.hurst@student.uq.edu.au>
2025-04-16 15:33:32 +1000

    Created menu for the engine

--- engine.py
+++ engine.py
@@ -1,45 +1,47 @@
 import pygame, sys, pymunk
 import apple, static_ball
-
+import main
 
 #HELLO THIS IS TEST LINE
-pygame.init() # init pygame
-screen = pygame.display.set_mode((800, 800)) #create display surface
-clock = pygame.time.Clock() #create game clock
-space = pymunk.Space()
-space.gravity = (10, 100) #0 horizontal, 500 vertical gravity
-apples = []
-apple_surface = pygame.image.load('images/apple_red.png')
 
-balls = []
+def engine():
+        
 
+    screen = pygame.display.set_mode((1280, 720)) #create display surface
+    clock = pygame.time.Clock() #create game clock
+    space = pymunk.Space()
+    space.gravity = (10, 100) #0 horizontal, 500 vertical gravity
+    apples = []
+    apple_surface = pygame.image.load('assets/apple_red.png')
 
-click_sound = pygame.mixer.Sound("sounds/iamsteve.mp3")
-
-
-
-while True: #Game loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            click_sound.play()
-            apples.append(apple.create_apple(space, event.pos))
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                mouse_pos = pygame.mouse.get_pos()
-                balls.append(static_ball.create_static_ball(space, mouse_pos))
-            if event.key == pygame.K_c:
-                for ball in balls:
-                    static_ball.remove_static_ball(space, ball)
-                balls.clear()
+    balls = []
 
 
-    
-    screen.fill((217, 217, 217)) #bg colour
-    apple.draw_apples(apples, screen, apple_surface)
-    static_ball.draw_static_balls(balls, screen)
-    space.step(1/50)
-    pygame.display.update() #render frame
-    clock.tick(120) #limit fps to 120
+    click_sound = pygame.mixer.Sound("assets/iamsteve.mp3")
+
+
+
+    while True: #Game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                main.main_menu()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click_sound.play()
+                apples.append(apple.create_apple(space, event.pos))
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    mouse_pos = pygame.mouse.get_pos()
+                    balls.append(static_ball.create_static_ball(space, mouse_pos))
+                if event.key == pygame.K_c:
+                    for ball in balls:
+                        static_ball.remove_static_ball(space, ball)
+                    balls.clear()
+
+
+        
+        screen.fill((217, 217, 217)) #bg colour
+        apple.draw_apples(apples, screen, apple_surface)
+        static_ball.draw_static_balls(balls, screen)
+        space.step(1/50)
+        pygame.display.update() #render frame
+        clock.tick(120) #limit fps to 120
